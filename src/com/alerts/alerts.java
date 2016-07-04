65cf60924a9dfea3ce0959667727388f9a4d3643
vishal2232 <vishalyadav.hbti@gmail.com>
2016-07-04 13:05:28 +0530

    AertDialogBox

---
+++ src/com/alerts/alerts.java
@@ -0,0 +1,14 @@
+package com.alerts;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class alerts extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
